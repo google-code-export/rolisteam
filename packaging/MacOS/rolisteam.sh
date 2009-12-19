573fb123edf79688fa320336806cb46fa0a3dd47
renaud.guezennec@gmail.com <renaud.guezennec@gmail.com@6f3f633c-eae7-11dd-9b7c-bbba4c9295f9>
2009-12-19 13:23:20 +0000

    little modification on the script

--- packaging/MacOS/rolisteam.sh
+++ packaging/MacOS/rolisteam.sh
@@ -5,10 +5,11 @@ dmg="dmg/rolisteam.dmg"
 
 rm -rf "$dir"
 mkdir "$dir"
+mkdir -p "dmg"
 cp -R bin/rolisteam.app "$dir"
 cp COPYRIGHT "$dir"
 cp AUTHORS "$dir"
-cp COPING.txt "$dir"
+cp COPYING.txt "$dir"
 cp -R resources/doc "$dir"/documentation
 hdiutil create -srcfolder "$dir" -volname "rolisteam" "$dmg"
 hdiutil internet-enable -yes "$dmg"
